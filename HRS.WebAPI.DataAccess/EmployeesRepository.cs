8fb9919b481b2c9253eb7215c72fb343903b70ae
RGonzalezs <evilholic@hotmail.com>
2017-03-27 09:51:47 -0500

    cambios

---
+++ HRS.WebAPI.DataAccess/EmployeesRepository.cs
@@ -0,0 +1,33 @@
+﻿using HRS.WebAPI.DataAccess.Model;
+using HRS.WebAPI.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HRS.WebAPI.DataAccess
+{
+    public class EmployeesRepository
+    {
+        public ResponseDto<List<EmployeesDto>> getAllJobs()
+        {
+            ResponseDto<List<EmployeesDto>> response = new ResponseDto<List<EmployeesDto>>();
+
+            using (HRStaffingModelConn objEntities = new HRStaffingModelConn())
+            {
+                IQueryable<EmployeesDto> query = (from jo in objEntities.JobsOffer
+                                                  select new EmployeesDto()
+                                                  {
+                                                      Id = jo.Id
+                                                    
+                                                  });
+
+                response.Data = query.ToList();
+
+                return response;
+            }
+
+        }
+    }
+}
